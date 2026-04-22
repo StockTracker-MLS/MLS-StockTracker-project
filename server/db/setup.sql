a40e74a9d2b5c1e9cc2744fb4bbf885ed3423907
Jonathan <JohnAnBer@gmail.com>
2026-04-22 15:31:12 -0400

    some sql work

--- server/db/setup.sql
+++ server/db/setup.sql
@@ -1,36 +1,28 @@
-DROP DATABASE IF EXISTS xx;
-CREATE DATABASE xx;
-\c xx;
+DROP DATABASE IF EXISTS project_db;
+CREATE DATABASE project_db;
+\c project_db;
 
-DROP TABLE IF EXISTS xx;
-DROP TABLE IF EXISTS xx;
-DROP TABLE IF EXISTS xx;
-DROP TABLE IF EXISTS xx;
-DROP TABLE IF EXISTS xx;
+DROP TABLE IF EXISTS user_bookmarks;
+DROP TABLE IF EXISTS bookmarks;
+DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS xx;
+-- DROP TABLE IF EXISTS xx;
 
 
-CREATE TABLE xx (
-  l,
-  l,
-  l,
-  l,
-  l
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  email VARCHAR(255) UNIQUE
 )
 
-CREATE TABLE xx (
-  l,
-  l,
-  l,
-  l,
-  l
+CREATE TABLE bookmarks (
+  bookmark_id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
 )
 
-CREATE TABLE xx (
-  l,
-  l,
-  l,
-  l,
-  l
+CREATE TABLE user_bookmarks (
+  user_bookmark_id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(user_id),
+  bookmark_id INT REFERENCES bookmarks(bookmark_id)
 )
 
 CREATE TABLE xx (
