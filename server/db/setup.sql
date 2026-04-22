fdf88117628e34dde40fc457bc37497f5aaee7ea
Jonathan <JohnAnBer@gmail.com>
2026-04-22 14:13:38 -0400

    sql started

---
+++ server/db/setup.sql
@@ -0,0 +1,56 @@
+DROP DATABASE IF EXISTS xx;
+CREATE DATABASE xx;
+\c xx;
+
+DROP TABLE IF EXISTS xx;
+DROP TABLE IF EXISTS xx;
+DROP TABLE IF EXISTS xx;
+DROP TABLE IF EXISTS xx;
+DROP TABLE IF EXISTS xx;
+
+
+CREATE TABLE xx (
+  l,
+  l,
+  l,
+  l,
+  l
+)
+
+CREATE TABLE xx (
+  l,
+  l,
+  l,
+  l,
+  l
+)
+
+CREATE TABLE xx (
+  l,
+  l,
+  l,
+  l,
+  l
+)
+
+CREATE TABLE xx (
+  l,
+  l,
+  l,
+  l,
+  l
+)
+
+CREATE TABLE xx (
+  l,
+  l,
+  l,
+  l,
+  l
+)
+
+INSERT INTO xx (l, l, l, l, l) VALUES (l, l, l, l, l);
+INSERT INTO xx (l, l, l, l, l) VALUES (l, l, l, l, l);
+INSERT INTO xx (l, l, l, l, l) VALUES (l, l, l, l, l);
+INSERT INTO xx (l, l, l, l, l) VALUES (l, l, l, l, l);
+INSERT INTO xx (l, l, l, l, l) VALUES (l, l, l, l, l);
